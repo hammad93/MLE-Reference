a36478263088b77a1b689c8646ed0536fce3edcf
hammad93 <hammadus@gmail.com>
2016-10-03 00:15:34 +0000

    Ariana Grande - Jasons Song

--- explore_enron_data.py
+++ explore_enron_data.py
@@ -18,6 +18,21 @@
 import pickle
 """
 pickle will make the object of type dict (dictionary)
+we will then have a dictionary of a dictionary
+
 """
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-print(len(enron_data.keys()))
+print "number of people: ", len(enron_data.keys())
+print "number of attributes per person: ", len(enron_data["SKILLING JEFFREY K"])
+
+"""
+The poi feature records whether the person is a person of interest, according to our definition. How many POIs are there in the E+F dataset?
+1) Iterate through all items in the dictionary
+2) Access the "poi" feature and add to count if true (or 1)
+data[person_name]["poi"]==1
+"""
+count = 0
+for i in enron_data:
+    if enron_data[i]["poi"] == 1 :
+        count+=1
+print "number of poi's: ", count
